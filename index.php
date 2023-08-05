ba94fcd282c7b6c429b0c1dfc0769e09b84f9632
vineji <119754682+vineji@users.noreply.github.com>
2023-08-05 01:05:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,180 @@
+<html>
+    <head>
+        <title>Vineji Sathananthan</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="reset.css" media="screen" />
+        <link rel="stylesheet" href="style.css" media="screen" />
+    </head>
+    <body>
+        <div class="container">
+            <header class="item1">
+                <nav>
+                    <ul>
+                        <li style="padding-right: 1.8rem; padding-top: 1rem;"><a href="viewBlog.php" class="item12">View blogs</a></li>
+                        <li style="padding-right: 1.8rem; padding-top: 1rem;"><a class="item12" href="session.php">Add post</a></li>
+                        <li style="padding-right: 1.8rem; padding-top: 1rem;"><a href="#contact" class="item12">Contact</a></li>
+                        <li style="padding-right: 1.8rem; padding-top: 1rem;"><a href="#home" class="item12">Home</a></li>
+                    </ul>
+                </nav>
+                <div class="side_bar">
+                    <ul>
+                        <li><a href="https://github.com/vineji" target="_blank" ><img class="sidebar_image_github" src="git.svg"></a></li>
+                        <li><a href="https://www.linkedin.com/in/sathananthan-vineji-273401259/" target="_blank"><img class="sidebar_image_linkedin" src="linkedin.svg"></a></li>
+                        <li><a href="CV.pdf" target="_blank"><img class="sidebar_image_resume" src="resume.svg" ></a></li>
+                        <li><a href="#contact"><img class="sidebar_image_email"  src="email.svg"></a></li>
+                    </ul>
+                </div>
+            </header>
+            <div class="item2" style="padding-top: 250px;" id="home">
+                <figure>
+                    <h0 class="item21">Hello, my name is</h0>
+                    <h1 class="item22">Vineji Sathananthan</h1>
+                </figure>
+                <figcaption class="item23">Undergraduate Computer Science student</figcaption>
+                <ul  class="item24">
+                    <li style="padding: 0.9rem;" ><a href ="#about" class="item25">About me</a></li>
+                    <li style="padding-left: 1rem;"></li>
+                    <li style="padding: 0.9rem;" ><a href="#education" class="item25">My education & skills</a></li>
+                    <li style="padding-left: 1rem;"></li>
+                    <li style="padding: 0.9rem;"><a href="#project" class="item25">My projects</a></li>
+                </ul>
+            </div>
+            <div id="about" class="move3"></div>
+            <div class="item3">
+                <h2 class="item31" >About me</h2>
+                <div class="item32">
+                    <h3 class="item33">Hello, I'm Vineji. Feel free to explore my website.</h3>
+                    <article class="item34" >As a BSc Computer Science student at Queen Mary University of London, I am focusing on web development while cultivating a keen interest in machine learning, recognizing its importance in shaping our future. During my leisure time, I actively acquire diverse programming languages, skillfully utilizing them to develop and enhance my professional portfolio.</article>
+                </div>
+            </div>
+            <div  class="move3"></div>
+            <div class="Econtainer" style="padding-top: 10rem;" id="education">
+                <header>
+                    <h1 class="E1">Education & Skills</h1>
+                </header>
+                <div class="tableContainer" >
+                    <table>
+                        <tr style="font-size: 1.5rem; color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th class="box-grade">GCSE </th>
+                            <th class="box" style="border-radius: 3rem 1.5rem 0rem 0rem;">Math</th>
+                            <th class="box" style="border-radius: 3rem 1.5rem 0rem 0rem;">English Literature</th>
+                            <th class="box" style="border-radius: 3rem 1.5rem 0rem 0rem;">English <br> Language</th>
+                            <th class="box" style="border-radius: 3rem 1.5rem 0rem 0rem;">Biology</th>
+                            <th class="box" style="border-radius: 3rem 1.5rem 0rem 0rem;">Chemistry</th>
+                            <th class="box" style="border-radius: 3rem 1.5rem 0rem 0rem;">Physics</th>
+        
+                        </tr>
+                        <tr style="font-size: 1.5rem; color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th class="box-grade">Grades</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">9</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">7</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">7</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">7</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">8</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">8</th>
+                        </tr>
+                        <tr style="font-size: 1.5rem;color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th></th>
+                            <th class="box1" style="border-radius: 3rem 1.5rem 0rem 0rem;">Computer Science</th>
+                            <th class="box1" style="border-radius: 3rem 1.5rem 0rem 0rem;">Geography</th>
+                            <th class="box1" style="border-radius: 3rem 1.5rem 0rem 0rem;">Art</th>
+                            <th class="box1" style="border-radius: 3rem 1.5rem 0rem 0rem;">French</th>
+                            <th class="box1" style="border-radius: 3rem 1.5rem 0rem 0rem;">Religious <br> Studies</th>
+                        </tr>
+                        <tr style="font-size: 1.5rem;color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th></th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">7</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">7</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">8</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">6</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">7</th>
+                        </tr>
+                    </table>
+                    <table>
+                        <tr style="font-size: 1.5rem; color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th class="box-grade-A-level">A-Level</th>
+                            <th class="box" style="border-radius: 4rem 1.5rem 0rem 0rem;">Math</th>
+                            <th class="box" style="border-radius: 4rem 1.5rem 0rem 0rem;">Further Math</th>
+                            <th class="box" style="border-radius: 4rem 1.5rem 0rem 0rem;">Chemistry</th>
+                        </tr>
+                        <tr style="font-size: 1.5rem; color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th class="box-grade-A-level">Grades</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">A*</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">A</th>
+                            <th class="box" style="border-radius: 0rem 0rem 1.5rem 1.5rem;">A</th>
+                        </tr>
+                    </table>
+                    <table style="padding-left: 20rem;">
+                        <tr style="font-size: 1.5rem; color: rgb(25, 41, 70); font-family: 'Abel', sans-serif;">
+                            <th class="box-grade" style="display: flex; justify-content: center;" >Skills</th>
+                            <th  class="box2"><img src="java2.png" class="image-box"></th>
+                            <th  class="box2" ><img src="python.png" class="image-box-python"></th>
+                            <th  class="box2"><img src="html1.png" class="image-box" style="padding-right: 1rem;"></th>
+                            <th  class="box2"><img src="css1.png" class="image-box" style="padding-right: 3rem;"></th>
+                            <th  class="box2" ><img src="php.png" class="image-box-php" style="padding-right: 2rem;"></th>
+                            <th  class="box2"><img src="sql.png" class="image-box"></th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div  class="move3"></div>
+            <div id="project"></div>
+            <div class="project" >
+                <h7  class="item51">Projects</h7>
+                <div  class="project_list">
+                    <div class="project_item">
+                        <h10 class="project_header">Portfolio Website</h10>
+                        <p class="project_text">This is my portfolio website to showcase my achievements and briefly describe myself and what I do when working on projects in the form of a blog. I used HTML, CSS, PHP and mySQL to build this website.</p>
+                        <div class="project_buttons" >
+                            <a class="Code" href='https://github.com/vineji/Portfolio-Website.git' target="_blank">Code</a>
+                        </div>
+                    </div>
+                    <div class="project_item">
+                        <h11 class="project_header">Football Quiz</h11>
+                        <p class="project_text">Test your football knowledge and your memorisation skills in this unique quiz. I have created this project with Java Swing code and implemented techniques such as polymorphism, abstraction and encapsulation from object orientated programming.</p>
+                        <div class="project_buttons">
+                            <a class="Code" href='https://github.com/vineji/Football-Quiz.git' target="_blank">Code</a>
+                            <a class="Demo" >Demo</a>
+                        </div>
+                    </div>
+                    <div class="project_item">
+                        <h12 class="project_header">Python Project</h12>
+                        <img class="work" src=work.png>
+                        <p></p>
+                        <a></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item7"  style="padding-top: 10cm;">
+                <h6 class="item71" style="padding-top: 3cm;" id="contact">Contact</h6>
+                <div style="padding-top: 4rem;"></div>
+                <ul class="item72" style="padding-top: 4rem;">
+                    <li>
+                        <p class="item73">Name</p>
+                        <div style="display: flex; flex-direction: row; gap: 1rem;">
+                            <p style="padding-top: 5px;" class="item74"></p>
+                        </div>    
+                    </li>
+                    <li>
+                        <p class="item73">Number</p>
+                        <p style="padding-top: 5px;" class="item74"></p>
+                    </li>
+                    <li style="padding-top: 0.8cm;">
+                        <p class="item73">Email</p>
+                        <p style="padding-top: 5px;" class="item74">vineji7@outlook.com</p>
+                    </li>
+                </ul>
+            </div>
+            <footer style="margin-top: 12rem;" >
+                <p class="footer_p">©2023 - Sathananthan Vineji </p>
+            </footer>
+
+        </div>
+    </body>
+</html>
