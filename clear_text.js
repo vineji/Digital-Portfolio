26d01e2a55bc05568a7a0732a1be382e88fbe0ff
Vineji Sathananthan <119754682+vineji@users.noreply.github.com>
2023-09-06 23:25:27 +0100

    Create clear_text.js

---
+++ clear_text.js
@@ -0,0 +1,11 @@
+function clear_text(){
+    document.getElementById('title').value = "";
+    document.getElementById('blog').value = "";
+    document.getElementById('blog').style.backgroundColor = "rgb(34, 50, 79)";
+    document.getElementById('title').style.backgroundColor = "rgb(34, 50, 79)";
+    document.getElementById('blog').style.borderColor = "rgb(34, 50, 79)";
+    document.getElementById('title').style.borderColor = "rgb(34, 50, 79)";
+
+}
+
+
